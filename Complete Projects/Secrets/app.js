0a50d8ef2b60d8207e79d189c4bf3334fbd1de1d
Malik Muhammad <MalikMuhammad@Maliks-MacBook-Air.local>
2019-09-09 17:32:10 -0700

    Level 1 + Level 2: Username and Password + Encrpytion & Env Variables

--- Complete Projects/Secrets/app.js
+++ Complete Projects/Secrets/app.js
@@ -1,26 +1,26 @@
 //jshint esversion:6
-require("dotenv").config();
+
+require(dotenv).config();
+const = express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
-const express = require("express");
-const ejs = require("ejs");
-const bodyParser = require("body-parser");
 
 const app = express();
 
-console.log(process.env.API_KEY);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+mongoose.connect("mongodb:/localhost:27017/userDB", {useNewUrlParser:true});
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
