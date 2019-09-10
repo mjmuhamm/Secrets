34c5e451aa94a7b02698af39309b0430f52d55b4
Malik Muhammad <MalikMuhammad@Maliks-MacBook-Air.local>
2019-09-10 16:42:42 -0700

    Change to mongodb atlas

--- Complete Projects/Secrets/app.js
+++ Complete Projects/Secrets/app.js
@@ -29,7 +29,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-malik:test123@mcluster-alnq7.mongodb.net/userDB", {useNewUrlParser:true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema ({
