1f75cd85e196f0d4825fb022e4bdf29766b6801d
Malik Muhammad <MalikMuhammad@Maliks-MacBook-Air.local>
2019-09-09 17:07:29 -0700

    Add Environment Files

--- Complete Projects/Secrets/app.js
+++ Complete Projects/Secrets/app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const express = require("express");
@@ -7,6 +8,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+console.log(process.env.API_KEY);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,8 +20,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret =" thisisalongsecretthatstatesIplayedgolfatacollegeIdon'ttellpeopleabout."
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
