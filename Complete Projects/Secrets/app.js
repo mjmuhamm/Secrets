206a3528a8a8070f8320185ab5f77c45b17e38f1
Malik Muhammad <MalikMuhammad@Maliks-MacBook-Air.local>
2019-09-09 19:38:38 -0700

    Level 3: Hashing | Delete Mongoose-Encryption and add Hashing with md5

--- Complete Projects/Secrets/app.js
+++ Complete Projects/Secrets/app.js
@@ -1,11 +1,11 @@
 //jshint esversion:6
 
-require(dotenv).config();
-const = express = require("express");
+require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -13,14 +13,12 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb:/localhost:27017/userDB", {useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
 
-const userSchema = new mongoose.Schema({
+const userSchema = {
   email: String,
   password: String
-});
-
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ["password"] });
+};
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +37,7 @@ app.get("/register", function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -53,7 +51,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if (!err){
